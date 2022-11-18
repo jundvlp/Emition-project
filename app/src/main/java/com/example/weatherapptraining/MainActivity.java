9f3db56237da71cf8b961858cda847e5358d1eb3
jundvlp <116437170+jundvlp@users.noreply.github.com>
2022-11-18 19:14:57 +0200

    add_button_settings

--- app/src/main/java/com/example/weatherapptraining/MainActivity.java
+++ app/src/main/java/com/example/weatherapptraining/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.weatherapptraining;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -16,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void button1(View view) {
+        System.out.print("clickonbutton1");
+        setContentView(R.layout.activity_main2);
     }
 }
