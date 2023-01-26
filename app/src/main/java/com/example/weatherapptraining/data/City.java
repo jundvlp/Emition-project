b4dead8ef9f7c160d91ecba0b407b48c1742213e
jundvlp <116437170+jundvlp@users.noreply.github.com>
2023-01-26 20:58:15 +0200

    setting class

--- app/src/main/java/com/example/weatherapptraining/data/City.java
+++ app/src/main/java/com/example/weatherapptraining/data/City.java
@@ -1,8 +1,41 @@
 package com.example.weatherapptraining.data;
 
-public class City {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.weatherapptraining.R;
+
+public class City extends AppCompatActivity {
    public String name;
+   public EditText etCity;
+   public Button btnGet;
+   public TextView tvResult;
     int maxTemperature;
     int minTemperature;
     // weather identifier - cloudy, sunny, ect
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.city_layout);
+
+        etCity= findViewById(R.id.etCity);
+        btnGet = findViewById(R.id.btnGet);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etCity.getText().toString();
+                tvResult.setText(name);
+            }
+            public void btnGet(View view) {
+                System.out.print("clickonbutton2");
+                setContentView(R.layout.detailed_forecast_layout);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/weatherapptraining/data/DetailedForecast.java
+++ app/src/main/java/com/example/weatherapptraining/data/DetailedForecast.java
@@ -1,8 +1,16 @@
 package com.example.weatherapptraining.data;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.weatherapptraining.R;
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
-public class DetailedForecast {
+public class DetailedForecast extends AppCompatActivity {
   public String name;
     // weather identifier - cloudy, sunny, ect;
     int temperature;
@@ -10,4 +18,22 @@ public class DetailedForecast {
     int humidity;
     int wind;
     ArrayList<Forecast> forecast;
+    public Button btnGet;
+    public ImageView cloud;
+    public TextView temp1;
+    public TextView tempview1;
+    public TextView Dnipro;
+    public ImageView cloud1;
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.detailed_forecast_layout);
+
+      btnGet= findViewById(R.id.btnGet);
+      cloud = findViewById(R.id.cloud);
+      temp1 = findViewById(R.id.temp1);
+      tempview1 = findViewById(R.id.tempview1);
+      Dnipro = findViewById(R.id.Dnipro);
+      cloud1 = findViewById(R.id.cloud1);
+      }
 }
